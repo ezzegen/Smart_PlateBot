2be8b2878457dafc7a7bc15a56211909889db0a4
ezzegen <nikaveronichka@yandex.ru>
2022-09-08 18:00:03 +0400

    added new function for censorship

--- bot.py
+++ bot.py
@@ -1,12 +1,19 @@
 import logging
+import string
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from config import TOKEN
+import json
 
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
+
+
+async def on_startup(_):
+    print('Бот вышел в онлайн')
+
 
 """КЛИЕНТСКАЯ ЧАСТЬ"""
 
@@ -33,5 +40,13 @@ async def restaurant_place(message: types.Message):
     await bot.send_message(message.from_user.id, 'ул. Печорская, д.5')
 
 
+@dp.message_handler()
+async def echo_send(message: types.Message):
+    if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split()}\
+            .intersection(set(json.load(open('censor.json')))) != set():
+        await message.reply('Мат в чате запрещен')
+        await message.delete()
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)

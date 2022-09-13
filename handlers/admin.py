6d47879d109263c960900556f1e7f841eeed0db8
ezzegen <nikaveronichka@yandex.ru>
2022-09-13 22:40:53 +0400

    added checking for admin rights

--- handlers/admin.py
+++ handlers/admin.py
@@ -2,7 +2,9 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import types, Dispatcher
 from aiogram.dispatcher.filters import Text
-from create_bot import dp
+from create_bot import bot, dp
+
+ID = None
 
 
 class FSMAdmin(StatesGroup):
@@ -12,9 +14,18 @@ class FSMAdmin(StatesGroup):
     price = State()
 
 
+# determining id of the current moderator
+async def make_changes_command(message: types.Message):
+    global ID
+    ID = message.from_user.id
+    await bot.send_message(message.from_user.id, 'Чем могу помочь, уважаемый администратор?')
+    await message.delete()
+
+
 async def start_fsm(message: types.Message):
-    await FSMAdmin.photo.set()
-    await message.reply('Загрузите фото.')
+    if message.from_user.id == ID:
+        await FSMAdmin.photo.set()
+        await message.reply('Загрузите фото.')
 
 
 async def load_photo(message: types.Message, state: FSMContext):
@@ -62,3 +73,4 @@ def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(load_price, state=FSMAdmin.price)
     dp.register_message_handler(cancel_handler, state="*", commands='отмена')
     dp.register_message_handler(cancel_handler, Text(equals='отмена', ignore_case=True), state="*")
+    dp.register_message_handler(make_changes_command, commads=['moderator'], is_chat_admin=True)

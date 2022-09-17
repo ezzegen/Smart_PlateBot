3d2e4821cd7c2986f85d2d852b0f0ba4c08c2b2f
ezzegen <nikaveronichka@yandex.ru>
2022-09-17 18:36:49 +0400

    connected datbase

--- data_base/sql_db.py
+++ data_base/sql_db.py
@@ -1,8 +1,9 @@
 import sqlite3 as sq
+from create_bot import bot
 
 
 def sql_start():
-    """Creating data base for 'menu'"""
+    """Creating database for 'menu'"""
     global base, cur
     base = sq.connect('smart_plate.db')
     cur = base.cursor()
@@ -13,6 +14,15 @@ def sql_start():
 
 
 async def sql_add_command(state):
+    """Writing the received data to the table"""
     async with state.proxy() as data:
         cur.execute('INSERT INTO menu VALUES(?, ?, ?, ?)', tuple(data.values()))
         base.commit()
+
+
+async def sql_read(message):
+    """Reading data for menu"""
+    for r in cur.execute('SELECT * FROM menu').fetchall():
+        await bot.send_photo(message.from_user.id, r[0],
+                             f'{r[1]}\nОписание: {r[2]}\n'
+                             f'Цена {r[-1]} руб.')
